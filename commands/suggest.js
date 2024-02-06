f4d2f430954a2d7960453b7bbc9fc0dc9770b17b
Dominik Fritsche <Dominik.Fritsche@outlook.de>
2024-02-06 21:27:26 +0100

    Hotfix suggest, returned object included a promise

--- commands/suggest.js
+++ commands/suggest.js
@@ -1,12 +1,16 @@
 exports.execute = {
     name:"suggest",
     code:(function suggest(args, standartargs){
+        console.log(standartargs);
         const util = require('../src/util.js')
         let list = args.message.split(" ");
         list.splice(0,1);
         list = list.join(" ");
         if (list && list != null){
-            standartargs.util_obj = util.saveIdea(list, standartargs.util_obj);
+            let obj = util.saveIdea(list, standartargs.util_obj);
+            if (obj != null){
+                standartargs.util_obj = obj;
+            }
             standartargs.socket.send(`PRIVMSG #${args.originChannel} :Saving your suggestion. Thank you for your help improving this bot luvv`);
         }else{
             standartargs.socket.send(`PRIVMSG #${args.originChannel} :/me Usage: !suggest <suggestion>`);
