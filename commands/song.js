22442e664aae44f4291fd6c378bdba6622f34c97
Dominik Fritsche <Dominik.Fritsche@outlook.de>
2024-02-16 13:36:16 +0100

    !song is now available while music cannot cannot be controlled

--- commands/song.js
+++ commands/song.js
@@ -2,9 +2,7 @@ exports.execute = {
     name:"song",
     code:(function song(args, standartargs){
         const spotify = require('../src/spotify.js');
-        if (standartargs.modify){
-            spotify.getCurrentSong(standartargs.socket, args.originChannel);
-        }
+        spotify.getCurrentSong(standartargs.socket, args.originChannel);
         return standartargs;
     })
 }
--- commands/swapmusikmodifier.js
+++ commands/swapmusikmodifier.js
@@ -1,7 +1,6 @@
 exports.execute = {
     name:"swapMusikModifier",
-    code:(function song(args, standartargs){
-        //returns current volume if input length > 1 while modifier is false
+    code:(function swapMusikModifier(args, standartargs){
         if (args.usernameSender == args.originChannel){
             standartargs.modify = !standartargs.modify;
             standartargs.socket.send(`PRIVMSG #${args.originChannel} :/me Set Modify-Musik to ${standartargs.modify}`)
